3d66a21996bd15cb33bae05b9bc88cf88daf388f
abou <abou@T2LY718.groupe-t2i.local>
2019-08-02 16:48:42 +0200

    ras

--- src/main/java/azur/support/webtool/configserver/ConfigServerApplication.java
+++ src/main/java/azur/support/webtool/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.mcommerce.configserver;
+package azur.support.webtool.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/azur/support/webtool/configserver/ConfigServerApplicationTests.java
+++ src/test/java/azur/support/webtool/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mcommerce.configserver;
+package azur.support.webtool.configserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
